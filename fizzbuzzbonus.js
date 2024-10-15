7ad70effba37c67dedaee8c5bb75c51a011201b6
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-10-15 16:52:06 +0200

    fix

--- fizzbuzzbonus.js
+++ fizzbuzzbonus.js
@@ -4,14 +4,13 @@ for(let i = 0; i<100; i++) {
     const rest5 = num%5===0;
     const firstWord = "Fizz";
     const secondWord = "Buzz";
+    let result = num;
     if(rest3 && rest5) {
         result = firstWord+secondWord;
     } else if(rest3) {
         result = firstWord;
     } else if(rest5) {
         result = secondWord;
-    } else {
-        result = num;
     }
     console.log(result);
 }
